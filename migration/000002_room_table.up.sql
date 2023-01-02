fcc3f10d1840053dc3da4a501d6fe1932fce4075
saurav <saurav.maharjan7@gmail.com>
2023-01-02 12:10:21 +0545

    chore : created migration file for Rooms

---
+++ migration/000002_room_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS rooms(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NULL,
+    deleted_at DATETIME NULL,
+    PRIMARY KEY(id)
+);
