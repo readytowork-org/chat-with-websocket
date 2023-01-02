0484efd2a77156fb07f1734f33bf41e17565fd80
saurav <saurav.maharjan7@gmail.com>
2023-01-02 12:12:09 +0545

    FIX : removed from services

--- api/services/services.go
+++ api/services/services.go
@@ -5,9 +5,5 @@ import "go.uber.org/fx"
 // Module exports services present
 var Module = fx.Options(
 	fx.Provide(NewFirebaseService),
-	fx.Provide(NewStorageBucketService),
 	fx.Provide(NewUserService),
-	fx.Provide(NewTwilioService),
-	fx.Provide(NewGmailService),
-	fx.Provide(NewS3BucketService),
 )
