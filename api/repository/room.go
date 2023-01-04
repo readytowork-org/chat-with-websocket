0cf5ac98b6af2df59e3880d1aa210fd04c3e6c4c
saurav <saurav.maharjan7@gmail.com>
2023-01-04 14:22:25 +0545

    created getRoomWithUser

--- api/repository/room.go
+++ api/repository/room.go
@@ -33,7 +33,12 @@ func (c RoomRepository) CreateRoom(Room models.Room) (models.Room, error) {
 	return Room, c.db.DB.Create(&Room).Error
 }
 
-// func (c RoomRepository) GetRoomWithUser(Room models.Room) error {
-// 	return c.db.DB.Model(&Room).Joins("Join user_rooms on rooms.id = user_rooms.room_id").
-// 		Where("user_rooms.user_id = ?", userId).Find(&models.UserRoom).Error
-// }
+func (c RoomRepository) GetRoomWithUser(userID int64) ([]models.Room, error) {
+
+	userRoom := []models.Room{}
+
+	queryBuilder := c.db.DB.Model(&models.Room{}).Joins("JOIN user_rooms on rooms.id = user_rooms.room_id").
+		Where("user_rooms.user_id = ?", userID).Find(&userRoom)
+	err := queryBuilder.Find(&userRoom).Error
+	return userRoom, err
+}
