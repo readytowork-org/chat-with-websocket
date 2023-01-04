b5f3725cb01fc294f084737cc96218ce72bb5154
saurav <saurav.maharjan7@gmail.com>
2023-01-04 11:57:17 +0545

    update : changed owenreId datatype

--- models/room.go
+++ models/room.go
@@ -3,7 +3,7 @@ package models
 type Room struct {
 	Base
 	Name    string `json:"name"`
-	OwnerId int    `json:"owner_id"`
+	OwnerId int64  `json:"owner_id"`
 }
 
 func (r Room) TableName() string {
