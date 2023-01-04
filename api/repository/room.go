b10ff89977ffd72e4a43da3869d713f7dc1e95da
saurav <saurav.maharjan7@gmail.com>
2023-01-04 11:58:39 +0545

    added with transaction function

--- api/repository/room.go
+++ api/repository/room.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"boilerplate-api/infrastructure"
 	"boilerplate-api/models"
+
+	"gorm.io/gorm"
 )
 
 type RoomRepository struct {
@@ -18,6 +20,20 @@ func NewRoomRepository(db infrastructure.Database,
 	}
 }
 
-func (c RoomRepository) CreateRoom(Room models.Room) error {
-	return c.db.DB.Create(&Room).Error
+func (c RoomRepository) WithTrx(trxHandle *gorm.DB) RoomRepository {
+	if trxHandle == nil {
+		c.logger.Zap.Error("Transaction Database not found in gin context. ")
+		return c
+	}
+	c.db.DB = trxHandle
+	return c
 }
+
+func (c RoomRepository) CreateRoom(Room models.Room) (models.Room, error) {
+	return Room, c.db.DB.Create(&Room).Error
+}
+
+// func (c RoomRepository) GetRoomWithUser(Room models.Room) error {
+// 	return c.db.DB.Model(&Room).Joins("Join user_rooms on rooms.id = user_rooms.room_id").
+// 		Where("user_rooms.user_id = ?", userId).Find(&models.UserRoom).Error
+// }
