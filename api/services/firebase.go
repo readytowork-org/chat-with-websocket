65199665bcefc7167b9de38acd41a731994bf694
saurav <saurav.maharjan7@gmail.com>
2023-01-10 17:17:26 +0545

    Fix : firebase user model, claims var

--- api/services/firebase.go
+++ api/services/firebase.go
@@ -23,12 +23,24 @@ func NewFirebaseService(
 	}
 }
 
-func (c FirebaseService) CreateUser(newUser models.User) (*auth.UserRecord, error) {
-	user := (&auth.UserToCreate{}).Email(newUser.Email).DisplayName(newUser.FullName)
+func (fb *FirebaseService) VerifyToken(idToken string) (*auth.Token, error) {
+	token, err := fb.fbAuth.VerifyIDToken(context.Background(), idToken)
+	return token, err
+}
+func (c FirebaseService) CreateUser(newUser models.FirebaseAuthUser) (*auth.UserRecord, error) {
+	user := (&auth.UserToCreate{}).Email(newUser.Email).DisplayName(newUser.DisplayName)
 	record, err := c.fbAuth.CreateUser(context.Background(), user)
 	if err != nil {
 		c.logger.Zap.Info(err)
 	}
-
+	claims := map[string]interface{}{
+		"role":   newUser.Role,
+		"fb_uid": record.UID,
+		"id":     newUser.UserId,
+	}
+	err = c.fbAuth.SetCustomUserClaims(context.Background(), record.UID, claims)
+	if err != nil {
+		return nil, err
+	}
 	return record, err
 }
