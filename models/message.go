c2c48395df6e770173a54ab006aa69ee84b3f328
saurav <saurav.maharjan7@gmail.com>
2023-01-19 16:51:39 +0545

    fix : datatype

--- models/message.go
+++ models/message.go
@@ -4,7 +4,7 @@ type Message struct {
 	Base
 	Text   string `json:"text"`
 	RoomId int64  `json:"room_id"`
-	UserId int64  `json:"user_id"`
+	UserId string `json:"user_id"`
 }
 
 func (m Message) TableName() string {
