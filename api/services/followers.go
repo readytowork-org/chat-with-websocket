cc1b4276629ad692d784af780f4f7333950fda80
saurav <saurav.maharjan7@gmail.com>
2023-01-25 14:34:14 +0545

    feat : created follower services with func's

---
+++ api/services/followers.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"boilerplate-api/api/repository"
+	"boilerplate-api/models"
+
+	"gorm.io/gorm"
+)
+
+type FollowersService struct {
+	repository repository.FollowersRepository
+}
+
+func NewFollowersService(
+	repository repository.FollowersRepository,
+) FollowersService {
+	return FollowersService{
+		repository: repository,
+	}
+}
+
+func (c FollowersService) WithTrx(trxHandle *gorm.DB) FollowersService {
+	c.repository = c.repository.WithTrx(trxHandle)
+	return c
+}
+
+func (c FollowersService) AddFollower(Follower models.Followers) (models.Followers, error) {
+	return c.repository.AddFollower(Follower)
+}
+
+func (c FollowersService) UnFollower(Follower models.Followers) (models.Followers, error) {
+	return c.repository.UnFollower(Follower)
+}
