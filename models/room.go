8089d5056a32b0a55c76d965b06c21e5a92fa37e
saurav <saurav.maharjan7@gmail.com>
2023-01-30 18:41:16 +0545

    feat : created join of user model

--- models/room.go
+++ models/room.go
@@ -4,6 +4,7 @@ type Room struct {
 	Base
 	Name      string `json:"name"`
 	IsPrivate bool   `json:"is_private"`
+	Users     []User `json:"users" gorm:"many2many:user_rooms;"`
 }
 
 func (r Room) TableName() string {
