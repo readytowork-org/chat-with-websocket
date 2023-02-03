20d56b6a21e3eaa6ef648b14ed8a2f8558a59dfb
saurav <saurav.maharjan7@gmail.com>
2023-02-03 14:50:13 +0545

    feat : created model with users

--- models/room.go
+++ models/room.go
@@ -4,7 +4,11 @@ type Room struct {
 	Base
 	Name      string `json:"name"`
 	IsPrivate bool   `json:"is_private"`
-	Users     []User `json:"users" gorm:"many2many:user_rooms;"`
+}
+
+type RoomWithUsers struct {
+	Room
+	Users []User `json:"users" gorm:"many2many:user_rooms;joinForeignKey:room_id"`
 }
 
 func (r Room) TableName() string {
