1a2636e78ff73ed95ed2576e96c5ff1c2fc80ada
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-02-08 02:43:11 +0545

    feat: constants

--- constants/context.go
+++ constants/context.go
@@ -1,5 +1,10 @@
 package constants
 
+import (
+	"github.com/gorilla/websocket"
+	"time"
+)
+
 const (
 	// DBTransaction is database transaction handle set at router context
 	DBTransaction = "db_trx"
@@ -17,5 +22,28 @@ const (
 	DUMMYEMAIL = "dummyrtw@mailinator.com"
 
 	//Adminrole ->
-	RoleAdmin= "admin"
+	RoleAdmin = "admin"
 )
+
+const (
+	// Max wait time when writing message to peer
+	WriteWait = 10 * time.Second
+
+	// Max time till next pong from peer
+	PongWait = 60 * time.Second
+
+	// Send ping interval, must be less then pong wait time
+	PingPeriod = (PongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	MaxMessageSize = 10000
+)
+
+var (
+	Newline = []byte{'\n'}
+)
+
+var WsUpgrade = websocket.Upgrader{
+	ReadBufferSize:  4096,
+	WriteBufferSize: 4096,
+}
