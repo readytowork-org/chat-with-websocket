fd1f7abadcef29382dc0f61e7ced2a40a3d0079c
Sawal Timsina <52816353+sawal-timsina@users.noreply.github.com>
2023-02-09 23:32:02 +0545

    fix: preload users in room

--- api/repository/room.go
+++ api/repository/room.go
@@ -35,19 +35,23 @@ func (c RoomRepository) CreateRoom(Room models.Room) (models.Room, error) {
 }
 
 func (c RoomRepository) GetRoomWithUser(userID string, cursor string) (userRooms []models.RoomWithUsers, err error) {
-	queryBuilder := c.db.DB.Model(&models.RoomWithUsers{}).Joins("JOIN user_rooms ON rooms.id = user_rooms.room_id").
-		Where("user_rooms.user_id IN (?)", c.db.DB.Model(&models.Followers{}).
-			Select("followers.user_id").
-			Where("followers.follow_user_id = ?", userID)).
-		Or("user_rooms.user_id IN (?)", c.db.DB.Model(&models.Followers{}).
-			Select("followers.follow_user_id").
-			Where("followers.user_id = ?", userID)).
-		Preload("Users")
-
+	queryBuilder := c.db.DB.Model(&models.RoomWithUsers{})
 	if cursor != "" {
-		time, _ := time.Parse(time.RFC3339, cursor)
-		queryBuilder = queryBuilder.Where("rooms.created_at < ?", time)
+		parsedCursor, _ := time.Parse(time.RFC3339, cursor)
+		queryBuilder = queryBuilder.Where("rooms.created_at < ?", parsedCursor)
 	}
+
+	queryBuilder = queryBuilder.
+		Select("rooms.*, F.follow_user_id, F.user_id").
+		Joins("LEFT JOIN user_rooms ON rooms.id = user_rooms.room_id").
+		Joins("LEFT JOIN followers F ON user_rooms.follower_id = F.id AND F.deleted_at IS NULL").
+		Where(c.db.DB.Where("F.user_id = ?", userID).Or("F.follow_user_id = ?", userID)).
+		Preload("Users", func(db *gorm.DB) *gorm.DB {
+			return db.Table("(?) as users", c.db.DB.Model(&models.RoomsUser{}).
+				Select("users.*, room_id").
+				Joins("JOIN followers F ON users.id = F.follow_user_id OR users.id = F.user_id").
+				Joins("LEFT JOIN user_rooms ur ON ur.follower_id = F.id"))
+		})
 
 	return userRooms, queryBuilder.Find(&userRooms).Limit(15).Error
 }
